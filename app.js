e17e1f2945805efb33f1736d93ff6ccc186733c6
deathrow76 <darr_mc76@hotmail.com>
2017-02-13 16:55:47 -0600

    Updated app.js
    changed path

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Deathrow on 2/13/17.
+ */
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Define the port to run on
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, 'DesignPortfolio')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+    console.log('Magic happens on port ' + port);
+});
