1d7bcb0739d2ff483c24fac5440fad22e64a1dfb
deathrow76 <darr_mc76@hotmail.com>
2017-02-15 08:41:15 -0600

    Took out unused html

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use(express.static(path.join(__dirname, '/')));
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
-    console.log('Magic happens on port ' + port);
+    console.log('Listening on localhost: ' + port);
 });
